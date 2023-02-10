6b4416cf36d6659f935a95cadeee35b78f40be2c
Vinay Kumar <110723566+squirtlelive@users.noreply.github.com>
2023-02-11 00:48:47 +0530

    Update settings.py

--- hms/settings.py
+++ hms/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_8^j*!lfydhge7gzl-jd_dq@3%6ydzmjwdvn52ee5(d5$m@4h='
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
