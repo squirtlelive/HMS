74b5d346d64427344c18605aa1617ef542c9b99c
squirtlelive <naa1.20284855@gmail.com>
2023-02-12 00:16:05 +0530

    commit

--- hms/urls.py
+++ hms/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from hospital import views
 
+####
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view),
