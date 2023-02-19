2a8670901a109b46891458ba51b20bc3eea305e9
squirtlelive <naa1.20284855@gmail.com>
2023-02-19 23:19:44 +0530

    commit

--- hms/settings.py
+++ hms/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+]
+INSTALLED_APPS += [
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
